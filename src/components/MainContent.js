f1806ca9ac5b6d5f562c97876db8a2e276639f66
Eduard Karesli <ed.karesli@gmail.com>
2020-01-28 18:00:16 +0100

    Changing the logic a bit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,7 +5,7 @@ class MainContent extends React.Component {
   constructor(props) {
     super(props);
     this.forcasts = [];
-    this.state = { allForcasts: [] };
+    this.state = { allForcasts: [] , modified: false };
     this.handleClick = this.handleClick.bind(this);
     this.renderForcast = this.renderForcast.bind(this);
     this.createForcast = this.createForcast.bind(this);
@@ -30,7 +30,7 @@ class MainContent extends React.Component {
       const weather_state_name = forcast.consolidated_weather[i].weather_state_name;
       const weather_state_abbr = forcast.consolidated_weather[i].weather_state_abbr;
       
-      LIs += '<li>' + `<h5>${date_of_forcast}</h5><br />` + `<img src="` + `https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg" width=60 height=60> <br />${weather_state_name} <br />
+      LIs += `<li key=${(date.getSeconds()+i).toString()}>` + `<h5>${date_of_forcast}</h5><br />` + `<img src="` + `https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg" width=60 height=60> <br />${weather_state_name} <br />
         Max: ${max_temp} &#8451;<br />
         Min: ${min_temp} &#8451;<br />`;
       if(i === 0) {
@@ -49,25 +49,27 @@ class MainContent extends React.Component {
     if(this.state.allForcasts.length < 1)
       return (<h3>No forcasts yet</h3>); 
     
-    let node = document.getElementById("searchResults");
-    let list = node.children;
-    for(let i = 0; i < list.length; i++) {
-      node.removeChild(list[list.length - 1 - i]);
-    }
+    // let node = document.getElementById("searchResults");
+    // let list = node.children;
+    // for(let i = 0; i < list.length; i++) {
+    //   node.removeChild(list[list.length - 1 - i]);
+    // }
     /*
     while(node.hasChildNodes()) {
       node.removeChild(node.childNodes[0]);
     }    
     */
+    let allElements = [];
     for(let i = 0; i < this.state.allForcasts.length; i++) {
-      let forcastElement = this.createForcast(this.state.allForcasts[i]); 
-      
-      let el = document.createElement('DIV');
-      el.innerHTML = forcastElement;
-      el.classList = "locationForcast";
-      document.getElementById("searchResults").appendChild(el);
+      let forcastElement = `<div>${this.createForcast(this.state.allForcasts[i])}</div>`; 
+
+      allElements.push(forcastElement);
+      // let el = document.createElement('DIV');
+      // el.innerHTML = forcastElement;
+      // el.classList = "locationForcast";
+      // document.getElementById("searchResults").appendChild(el);
     }
-    
+    return allElements;
    
     /*
     let city = this.state.allForcasts[0].city;
@@ -114,16 +116,18 @@ class MainContent extends React.Component {
     // let res = {};
     
     // let woeid = '';
-        
-    fetch('http://localhost:5000/forcasts/' + locName + '/')
+    const url = `http://localhost:5000/forcasts/${locName}/`;    
+    fetch(url)
       .then(response => { return response.json(); })
-        .then(data => { console.log(data); 
+        .then(data => { 
+         // console.log("Data from server: ", data); 
           if(this.forcasts.length > 5)
           {
             this.forcasts.pop();            
           }
           this.forcasts.push({ city: locName, consolidated_weather: data.consolidated_weather});
           this.setState({allForcasts: this.forcasts});          
+         // console.log("this.state: ", this.state.allForcasts)
         })
           .catch(err => {
             console.log(err);
@@ -132,13 +136,13 @@ class MainContent extends React.Component {
 
   render() {
     return(
-      <main>
+      <div>
         <div>
           <input type="text" name="search" placeholder="Search for location"/> 
           <button type="button" name="button" value="getWeather" onClick={this.handleClick}>Get Weather</button>        
         </div>
-        <div id="searchResults">{this.renderForcast()}</div>
-      </main>
+        <this.renderForcast/>
+      </div>
     );
   }
 }
